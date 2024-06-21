7829199adda3fbca7e7a597e4a8768369759bd9d
Vinijs <vini-spider-6@hotmail.com>
2024-06-21 18:53:05 -0300

    Aula 14 - 21/06/2024

--- Viagens.TorneSeUmProgramador.App/Views/Login.xaml.cs
+++ Viagens.TorneSeUmProgramador.App/Views/Login.xaml.cs
@@ -2,14 +2,26 @@ namespace Viagens.TorneSeUmProgramador.App.Views;
 
 public partial class Login : ContentPage
 {
+	private bool senhaVisivel = false;
+
 	public Login()
 	{
 		InitializeComponent();
 	}
 
-    private void OnClickNavegarParaPaginaHome(object sender, EventArgs e)
+    private void SenhaImage_Tapped(object sender, TappedEventArgs e)
     {
-		//Navegação absoluta
-		Shell.Current.GoToAsync("//home");
+		if (senhaVisivel)
+		{
+            SenhaEntry.IsPassword = true;
+            SenhaImage.Source = ImageSource.FromFile("eyeclosed.png");
+            senhaVisivel = false;
+        }
+		else
+		{
+            SenhaEntry.IsPassword = false;
+            SenhaImage.Source = ImageSource.FromFile("eyeopen.png");
+            senhaVisivel = true;
+        }
     }
 }
