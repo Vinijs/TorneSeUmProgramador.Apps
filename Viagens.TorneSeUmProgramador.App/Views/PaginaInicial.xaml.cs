5e70966dc2aed6003b823a83583985d17c3bea47
Vinijs <vini-spider-6@hotmail.com>
2024-06-24 00:36:09 -0300

    Aula 15 - 23/06/2024

--- Viagens.TorneSeUmProgramador.App/Views/PaginaInicial.xaml.cs
+++ Viagens.TorneSeUmProgramador.App/Views/PaginaInicial.xaml.cs
@@ -5,11 +5,39 @@ public partial class PaginaInicial : ContentPage
 	public PaginaInicial()
 	{
 		InitializeComponent();
-	}
 
-    private void OnClickNavegarParaPassagensAereas(object sender, EventArgs e)
-    {
-		//Navegação relativa
-		Shell.Current.GoToAsync("/busca-passagem-aerea");
-    }
+		CarouselViewPrincipal.ItemsSource = new List<object>
+		{
+			new
+			{
+				Imagem = "https://images.unsplash.com/photo-1490644658840-3f2e3f8c5625?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+				Titulo = "Nova York",
+				Descricao = "Distância 2500 - mi"
+			},
+            new
+            {
+                Imagem = "https://images.unsplash.com/photo-1490644658840-3f2e3f8c5625?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+                Titulo = "Nova York",
+                Descricao = "Distância 2500 - mi"
+            },
+            new
+            {
+                Imagem = "https://images.unsplash.com/photo-1490644658840-3f2e3f8c5625?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+                Titulo = "Nova York",
+                Descricao = "Distância 2500 - mi"
+            },
+            new
+            {
+                Imagem = "https://images.unsplash.com/photo-1490644658840-3f2e3f8c5625?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+                Titulo = "Nova York",
+                Descricao = "Distância 2500 - mi"
+            },
+            new
+            {
+                Imagem = "https://images.unsplash.com/photo-1490644658840-3f2e3f8c5625?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+                Titulo = "Nova York",
+                Descricao = "Distância 2500 - mi"
+            }
+        };
+	}
 }
